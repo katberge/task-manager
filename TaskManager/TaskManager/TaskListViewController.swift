ce1a24dfa3626e096e2d05258940b3754baf4bbf
Kat Berge <kat.n.berge@gmail.com>
2020-11-23 18:25:17 -0600

    adds table veiw set up int TaskTableViewController

--- TaskManager/TaskManager/TaskListViewController.swift
+++ TaskManager/TaskManager/TaskListViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class TaskListViewController: UITableViewController {
+    var tasks: [Task] = []
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row].content
+        return cell
     }
 
 }
