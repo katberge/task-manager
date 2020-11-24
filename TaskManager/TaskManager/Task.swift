a258b66b87dc53748d8e1cab2c8bb78b0db88133
Kat Berge <kat.n.berge@gmail.com>
2020-11-23 19:16:58 -0600

    changes story board and Task structs to accept steps to each task and adds TaskManager struct to handle sqlite3 interaction

--- TaskManager/TaskManager/Task.swift
+++ TaskManager/TaskManager/Task.swift
@@ -6,8 +6,39 @@
 //
 
 import Foundation
+import SQLite3
 
 struct Task {
-    let content: String
+    let title: String
+    let steps: [Step]
+}
+
+struct Step {
+    let contents: String
     var completed: Bool
+}
+
+class TaskManager {
+    var database: OpaquePointer?
+    
+    func setUpDatabase() {
+        do  {
+            let databaseURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("tasks.sqlite3")
+            
+            if sqlite3_open(databaseURL.path, &database) == SQLITE_OK {
+                if sqlite3_exec(database, "CREATE TABLE IF NOT EXIST tasks (title TEXT)", nil, nil, nil) != SQLITE_OK {
+                    print("Error creating tasks table")
+                }
+                if sqlite3_exec(database, "CREATE TABLE IF NOT EXIST steps (task_id INT, contents TEXT, completed INT)", nil, nil, nil) != SQLITE_OK {
+                    print("Error creating tasks table")
+                }
+            }
+            else {
+                print("Error connecting")
+            }
+        }
+        catch let error {
+            print(error)
+        }
+    }
 }
